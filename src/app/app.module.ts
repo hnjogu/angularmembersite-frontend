ed40fd6a006fda9be69c72e7f7d225d508d6c3dd
root <root@localhost.localdomain>
2017-05-07 17:47:54 +0300

    updated

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { ComponentNameComponent } from './component-name/component-name.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ComponentNameComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
